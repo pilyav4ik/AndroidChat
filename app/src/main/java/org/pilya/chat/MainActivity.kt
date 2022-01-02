5b54bc4e97a6186b135ba719b51335e4b4472177
Igor Piliavskyi <ipiliavskyi@mail.com>
2022-01-02 13:16:05 +0100

    init project

---
+++ app/src/main/java/org/pilya/chat/MainActivity.kt
@@ -0,0 +1,24 @@
+package org.pilya.chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+
+import com.google.firebase.database.FirebaseDatabase
+
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Write a message to the database
+        // Write a message to the database
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
+    }
+}
